78996811471d80c8690fb7c0e5e106f4a1b9908e
Anastasiya Papko <anastasiyapapko@gmail.com>
2020-07-21 13:54:02 +0200

    Updates

---
+++ precache-manifest.c1d207043ba15f0741ce26922361a341.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e8aa4a55fd778ea451b2c7e18dcfdbf",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "4308cbba037fae90833e",
+    "url": "/project/static/css/main.1f649506.chunk.css"
+  },
+  {
+    "revision": "810b7332662199262c88",
+    "url": "/project/static/js/2.4248ace2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/project/static/js/2.4248ace2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4308cbba037fae90833e",
+    "url": "/project/static/js/main.8757e047.chunk.js"
+  },
+  {
+    "revision": "018ee7d753c580579f5a",
+    "url": "/project/static/js/runtime-main.245d0aa8.js"
+  }
+]);
